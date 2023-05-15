ab4a7a2bf4998077c3a4941cc1ea10410b9c7137
vikkol04 <vikkol04@gmail.com>
2023-05-15 21:06:30 +0300

    15.05.2023

---
+++ app/src/main/java/com/example/kolokolnikov_android_studio_second_course/Activity_pr_1.kt
@@ -0,0 +1,55 @@
+package com.example.kolokolnikov_android_studio_second_course
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class Activity_pr_1 : AppCompatActivity() {
+    var msgOnStart = true
+    var msgOnResume = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("pr_1", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (this.msgOnStart) {
+            Log.d("pr_1", "onStart")
+            this.msgOnStart = false
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (this.msgOnResume) {
+            Log.d("pr_1", "onResume")
+            this.msgOnResume = false
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("pr_1", "(onPause)")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("pr_1", "(onDestroy)")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("pr_1", "(onStop)")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("pr_1", "(onRestart)")
+        Log.d("pr_1", "App is running")
+    }
+
+}
--- app/src/main/java/com/example/kolokolnikov_android_studio_second_course/MainActivity.kt
+++ app/src/main/java/com/example/kolokolnikov_android_studio_second_course/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.kolokolnikov_android_studio_second_course
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MyLogAct", "onCreate(Main)")
+        val intentPr1 = Intent(this, Activity_pr_1::class.java)
+        startActivity(intentPr1)
+
     }
 }
